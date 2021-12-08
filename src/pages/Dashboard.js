67cc113612cd3e769afa64076ee59dcf00cc36fe
guangyuhe <fredhgy2010@gmail.com>
2021-12-08 18:04:47 +0100

    working with dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,6 +5,12 @@ const Dashboard = () => {
   const userInput = useRef();
   const passInput = useRef();
 
+  const name_input = useRef();
+  const geo_input = useRef();
+  const type_input = useRef();
+  const review_input = useRef();
+  const review_details_input = useRef();
+
   //SECTION control sidebar component state
   const [correct_display, set_correct_display] = useState(false);
   const intervalRef_correct_display = useRef();
@@ -26,25 +32,137 @@ const Dashboard = () => {
     ) {
       change_correct_display(false);
       //window.open('https://www.google.com', '_blank');
-      window.location.replace(
+      /*window.location.replace(
         "https://asianfood.heguangyu.net/data_manage.html"
-      );
+      );*/
     } else {
       change_correct_display(true);
     }
   };
 
-  let geo_name = "";
+  let marker_pos = "";
+  let info_name = "";
+  let type = "";
+  let review = 0;
+  let review_details = "";
+
   const location = useLocation();
   if (location.state === null) {
   } else {
-    console.log(location.state);
-    geo_name = location.state.geo_name;
+    marker_pos = {
+      lat: parseFloat(location.state.lat),
+      lng: parseFloat(location.state.lng),
+    };
+    info_name = location.state.geo_name;
+    type = location.state.type_name;
+    review = parseInt(location.state.review_points);
+    review_details = location.state.review_text;
   }
+
+  //SECTION control sidebar component state
+  const [alert_name_input, set_alert_name_input] = useState(false);
+  const intervalRef_alert_name_input = useRef();
+  function change_alert_name_input(props) {
+    setTimeout(() => {
+      set_alert_name_input(props);
+    }, 100);
+  }
+  useEffect(() => {
+    intervalRef_alert_name_input.current = alert_name_input;
+  }, [alert_name_input]);
+  //!SECTION
+
+  //SECTION control sidebar component state
+  const [alert_geo_input, set_alert_geo_input] = useState(false);
+  const intervalRef_alert_geo_input = useRef();
+  function change_alert_geo_input(props) {
+    setTimeout(() => {
+      set_alert_geo_input(props);
+    }, 100);
+  }
+  useEffect(() => {
+    intervalRef_alert_geo_input.current = alert_geo_input;
+  }, [alert_geo_input]);
+  //!SECTION
+
+  //SECTION control sidebar component state
+  const [alert_type_input, set_alert_type_input] = useState(false);
+  const intervalRef_alert_type_input = useRef();
+  function change_alert_type_input(props) {
+    setTimeout(() => {
+      set_alert_type_input(props);
+    }, 100);
+  }
+  useEffect(() => {
+    intervalRef_alert_type_input.current = alert_type_input;
+  }, [alert_type_input]);
+  //!SECTION
+
+  //SECTION control sidebar component state
+  const [alert_review_input, set_alert_review_input] = useState(false);
+  const intervalRef_alert_review_input = useRef();
+  function change_alert_review_input(props) {
+    setTimeout(() => {
+      set_alert_review_input(props);
+    }, 100);
+  }
+  useEffect(() => {
+    intervalRef_alert_review_input.current = alert_review_input;
+  }, [alert_review_input]);
+  //!SECTION
+
+  //SECTION work with open in google
+  const [gmap_link_name, set_gmap_link_name] = useState("");
+  const intervalRef_gmap_link_name = useRef();
+  function change_gmap_link_name(props) {
+    setTimeout(() => {
+      set_gmap_link_name(props);
+    }, 100);
+  }
+  useEffect(() => {
+    intervalRef_gmap_link_name.current = gmap_link_name;
+  }, [gmap_link_name]);
+  const handelChange_name_input = (event) => {
+    change_gmap_link_name(event.target.value);
+  };
+  //!SECTION
+
+  const handelClick_submit = (event) => {
+    console.log(name_input.current.value);
+    if (name_input.current.value === "") {
+      change_alert_name_input(true);
+    } else {
+      change_alert_name_input(false);
+    }
+    if (geo_input.current.value === "") {
+      change_alert_geo_input(true);
+    } else {
+      change_alert_geo_input(false);
+    }
+    if (type_input.current.value === "") {
+      change_alert_type_input(true);
+    } else {
+      change_alert_type_input(false);
+    }
+    if (review_input.current.value === "") {
+      change_alert_review_input(true);
+    } else {
+      change_alert_review_input(false);
+    }
+  };
+
+  const handelClick_clear = (event) => {
+    change_gmap_link_name("");
+    name_input.current.value = "";
+    geo_input.current.value = "";
+    type_input.current.value = "";
+    review_input.current.value = "";
+    review_details_input.current.value = "";
+  };
 
   return (
     <Fragment>
-      <div className="hidden absolute inset-96">
+      <div className={`${correct_display ? "" : "hidden"} absolute inset-96`}>
         <h2>Dashboard</h2>
         <div>
           <div>
@@ -81,8 +199,88 @@ const Dashboard = () => {
           {correct_text}{" "}
         </p>
       </div>
-      <div className="absolute inset-96">
-        <p>Name:{geo_name}</p>
+      <div
+        className={`${
+          correct_display ? "hidden" : ""
+        } fixed bg-gray-100 inset-x-0 inset-y-12`}
+      >
+        <div className="flow-root p-1">
+          <p className="inline">Name:</p>
+          <a
+            href={`https://maps.google.co.in/maps?q=${gmap_link_name}`}
+            target="_blank"
+            rel="noreferrer"
+            className="text-purple-400 antialiased inline items-end"
+          >
+            <span className="text-blue-600">Open </span>
+            <span className="text-red-600">in </span>
+            <span className="text-yellow-600">Google</span>
+          </a>
+          <input
+            className={`${
+              alert_name_input ? "border-2 border-red-500" : ""
+            } w-80 block`}
+            defaultValue={info_name}
+            ref={name_input}
+            onInput={handelChange_name_input}
+          ></input>
+        </div>
+        <div className="flow-root p-1">
+          <p>Geo Info:</p>
+          <input
+            className={`${
+              alert_geo_input ? "border-2 border-red-500" : ""
+            } w-80`}
+            defaultValue={marker_pos.lat + "," + marker_pos.lat}
+            ref={geo_input}
+          ></input>
+        </div>
+        <div className="flow-root p-1">
+          <p>Type:</p>
+          <input
+            className={`${
+              alert_type_input ? "border-2 border-red-500" : ""
+            } w-80`}
+            defaultValue={type}
+            ref={type_input}
+          ></input>
+        </div>
+        <div className="flow-root p-1">
+          <p>Review:</p>
+          <input
+            className={`${
+              alert_review_input ? "border-2 border-red-500" : ""
+            } w-80`}
+            defaultValue={review}
+            ref={review_input}
+          ></input>
+        </div>
+        <div className="flow-root p-1">
+          <p>Review details:</p>
+          <input
+            className={`w-80 h-12`}
+            defaultValue={review_details}
+            ref={review_details_input}
+          ></input>
+        </div>
+        <div className="py-2">
+          <button
+            type="submit"
+            onClick={handelClick_submit}
+            className="group relative w-80 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            submit
+          </button>
+        </div>
+        <div>
+          <button
+            type="submit"
+            onClick={handelClick_clear}
+            className="group relative w-80 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            clear
+          </button>
+        </div>
       </div>
     </Fragment>
   );
