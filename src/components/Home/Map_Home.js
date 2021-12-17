dfc45601072bfdc923b134a690341af1614a21c2
guangyuhe <fredhgy2010@gmail.com>
2021-12-17 22:08:54 +0100

    map fly to finished

--- src/components/Home/Map_Home.js
+++ src/components/Home/Map_Home.js
@@ -1,21 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 
 const Map = (props) => {
-  const [position, set_position] = useState([
-    52.517674728732054, 13.393789389208452,
-  ]);
+  const Golocation = (props) => {
+    const [position, set_position] = useState({
+      lat: 52.517674728732054,
+      lng: 13.393789389208452,
+    });
 
-  return (
-    <MapContainer center={position} zoom={13} zoomControl={false}>
+    useEffect(() => {
+      if (props.geo.lat === 0) {
+      } else {
+        let new_position = {
+          lat: parseFloat(props.geo.lat),
+          lng: parseFloat(props.geo.lng),
+        };
+        console.log(new_position);
+        set_position(new_position);
+      }
+    }, [props.geo]);
+    const map = useMap();
+    map.flyTo(position, map.getZoom());
+    return (
       <Marker position={position}>
         <Popup>{props.geo.geo_name}</Popup>
       </Marker>
+    );
+  };
 
+  return (
+    <MapContainer
+      center={{
+        lat: 52.517674728732054,
+        lng: 13.393789389208452,
+      }}
+      zoom={13}
+      zoomControl={false}
+    >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Golocation geo={props.geo}></Golocation>
     </MapContainer>
   );
 };
