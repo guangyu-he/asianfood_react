157b79fb45bb3f582cf3c8f25ba66af82379e45b
guangyuhe <fredhgy2010@gmail.com>
2021-12-17 14:39:17 +0100

    change app to functional component

--- src/App.js
+++ src/App.js
@@ -12,27 +12,26 @@ import ReactGA from "react-ga";
 ReactGA.initialize("UA-203426892-2");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        {/* Routes nest inside one another. Nested route paths build upon
-                parent route paths, and nested route elements render inside
-                parent route elements. See the note about <Outlet> below. */}
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Dashboard" element={<Dashboard />} />
+const App = React.memo((props) => {
+  return (
+    <Fragment>
+      {/* Routes nest inside one another. Nested route paths build upon
+              parent route paths, and nested route elements render inside
+              parent route elements. See the note about <Outlet> below. */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
 
-            {/* Using path="*"" means "match anything", so this route
-                      acts like a catch-all for URLs that we don't have explicit
-                      routes for. */}
-            <Route path="*" element={<NoMatch />} />
-          </Route>
-        </Routes>
-      </Fragment>
-    );
-  }
-}
+          {/* Using path="*"" means "match anything", so this route
+                  acts like a catch-all for URLs that we don't have explicit
+                  routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </Fragment>
+  );
+});
+
 export default App;
