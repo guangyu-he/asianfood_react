faa96de6f1dfecc60131548fcc88b6f44257ec83
guangyuhe <fredhgy2010@gmail.com>
2021-12-19 16:16:57 +0100

    fix update php bug

--- public/update_location_react.php
+++ public/update_location_react.php
@@ -9,7 +9,7 @@ $dbhost = 'localhost';  // mysql服务器主机地址
 $dbuser = 'test';            // mysql用户名
 //$dbpass = '0';          // mysql用户名密码
 $dbname = 'my_db';          // mysql用户名密码
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+$conn_inset = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 // 检测连接
 if (!$conn_inset) {
