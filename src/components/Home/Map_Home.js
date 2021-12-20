13a49a9ebcb0977ca3589b8fb93d22e780f35d70
guangyuhe <fredhgy2010@gmail.com>
2021-12-20 21:52:29 +0100

    map dark mode support

--- src/components/Home/Map_Home.js
+++ src/components/Home/Map_Home.js
@@ -47,6 +47,7 @@ const Map = (props) => {
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        className= 'map-tiles'
       />
       <Golocation geo={props.geo}></Golocation>
     </MapContainer>
