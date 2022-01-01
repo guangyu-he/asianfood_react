f092d8286cc6babba7b8ffedf5ae0e9d1173ad89
fredhgy <github@heguangyu.net>
2022-01-01 21:54:04 +0100

    fix home button in sidebar bug

--- src/components/Layout/Homebutton_Layout.js
+++ src/components/Layout/Homebutton_Layout.js
@@ -11,8 +11,9 @@ const Homebutton = React.memo((props) => {
     home_selected = true;
   }
   return (
-    <button
-      className={`${
+    <Link
+      to="/"
+      lassName={`${
         home_selected
           ? "bg-gray-200 border-gray-400 dark:bg-gray-500 dark:border-gray-300"
           : "hover:bg-gray-300"
@@ -24,10 +25,8 @@ const Homebutton = React.memo((props) => {
         `}
       onClick={() => props.change_sidebar_item_selected("home")}
     >
-      <Link to="/">
-        <span className="mx-4 font-medium dark:text-white">Home</span>
-      </Link>
-    </button>
+      <span className="mx-4 font-medium dark:text-white">Home</span>
+    </Link>
   );
 });
 
