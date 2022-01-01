51c20d61e217caf6c81007fbdc05eb6d9ed63289
fredhgy <github@heguangyu.net>
2022-01-01 21:55:52 +0100

    typo mistake fixed

--- src/components/Layout/Homebutton_Layout.js
+++ src/components/Layout/Homebutton_Layout.js
@@ -13,7 +13,7 @@ const Homebutton = React.memo((props) => {
   return (
     <Link
       to="/"
-      lassName={`${
+      className={`${
         home_selected
           ? "bg-gray-200 border-gray-400 dark:bg-gray-500 dark:border-gray-300"
           : "hover:bg-gray-300"
